76e8d0af90f84894dfa90467d73f304bd6e09e5c
Michel Laporte <michel.laporte@essencedigital.com>
2017-05-02 16:57:49 +0100

    Update nat.sh

--- nat.sh
+++ nat.sh
@@ -1,4 +1,4 @@
-read -p "Press [enter] to update NTP..."
-sudo apt-get update
-sudo apt-get install ntp
-dpkg -s ntp | grep Version
+#read -p "Press [enter] to update NTP..."
+sleep 10
+#sudo apt-get install ntp
+#dpkg -s ntp | grep Version
