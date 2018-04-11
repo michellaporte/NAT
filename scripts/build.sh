be0a824a294d75b82e395a27c642baf7c025b121
Michel Laporte <michel.laporte@essenceglobal.com>
2018-04-11 16:12:11 +0100

    -

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 HOST=104.155.81.36
 #gcloud compute instances list --format="value(selfLink.basename())"
+#--filter="name:datalake-stage-'*'"
 #gcloud compute ssh "${HOST_INSTANCE}" --strict-host-key-checking=no --zone europe-west1-c --command "touch /home/michel.laporte/\"${SHIT}\""
 for i in $(gcloud compute instances list --format="value(selfLink.basename())")
 do
